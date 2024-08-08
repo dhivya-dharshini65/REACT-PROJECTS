d7712924d7021438731da9375b1616c8445ad435
dhivya-dharshini65 <727723eucs055@skcet.ac.in>
2024-08-08 15:59:03 +0530

    Add files via upload

---
+++ DAY2/d2cy1.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+function Smile()
+{
+    return(
+        <div style={{background:"lightgrey",backgroundSize:"100px"}}>
+            <h1 style={{textAlign:"center"}}>Smile Component</h1>
+            <p style={{textAlign:"center"}}>It is a function component</p>
+            <br></br>
+            <img src="https://tse4.explicit.bing.net/th?id=OIP.6es2MCaxiJLH0OxW3ixgWgHaHa&pid=Api&P=0&h=180" style={{paddingLeft:"550px",height:"200px",width:"200px"}}/>
+        </div>
+    );
+}
+export default Smile;
